35340c129f8328ce30db7684cc505431dc7eaa2a
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-10-07 14:53:54 +0200

    Soluzione ex7 - Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'eos-login',
@@ -10,6 +11,11 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showValue(f: NgForm) {
+    alert(JSON.stringify(f.value));
+    f.reset();
   }
 
 }
